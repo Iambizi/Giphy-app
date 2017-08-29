b84c9fa44a284d194c4ae15296155802d56b23e6
Amir Bizimana <amirbzmn@gmail.com>
2017-08-28 23:38:40 -0400

    added trending button functionality
    and added limit object to giphyOptions

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     const giphyOptions = {
       q: word,
       api_key: `e729134c03124b8e9ada09ff218cd33b`,
+      limit: 35,
       fmt: `json`
     };
     function displayGifs(data) {
@@ -17,4 +18,23 @@ $(document).ready(function() {
     }
    $.getJSON(giphyAPI,giphyOptions,displayGifs); 
   });
+  $('.trending').click(function(){
+  const trendyGiph = `https://api.giphy.com/v1/gifs/trending?`;
+ 
+   const giphyOptions = {
+    api_key: `e729134c03124b8e9ada09ff218cd33b`,   
+    limit: 35,
+    rating:"g",
+    fmt: `json`, 
+   };
+ 
+  function displayGifs(data) {
+      let gifHTML = `<div>`;
+       $.each(data.data, function(i,gif){
+       gifHTML += `<a href= ${gif.url}><img src=${gif.images.fixed_width.url} class="item" height=100px ></a>`;
+      });
+      gifHTML += `</div>`;
+      $(`.gifs-area`).html(gifHTML);
+    }  $.getJSON(trendyGiph,giphyOptions,displayGifs)
+ });
 });
